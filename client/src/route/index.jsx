51e1064822a2704a443fa3ff11e8dc07997edaf8
Miraculous20 <miraculous15052003@gmail.com>
2025-08-17 00:11:56 +0100

    fix: updates

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -1,52 +1,52 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // --- CORE & LAYOUTS ---
-import App from "/src/App";
-import Dashboard from "/src/layouts/Dashboard";
-import AdminPermission from "/src/layouts/AdminPermission";
-import AgentPermission from "/src/layouts/AgentPermission";
-import EateryAdminPermission from "/src/layouts/EateryAdminPermission";
+import App from "/src/App.jsx";
+import Dashboard from "/src/layouts/Dashboard.jsx";
+import AdminPermission from "/src/layouts/AdminPermission.jsx";
+import AgentPermission from "/src/layouts/AgentPermission.jsx";
+import EateryAdminPermission from "/src/layouts/EateryAdminPermission.jsx";
 
 // --- GENERAL PAGES ---
-import Home from "/src/pages/Home";
-import AboutPage from "/src/pages/About";
-import SearchPage from "/src/pages/SearchPage";
-import CategoryProductListPage from "/src/pages/CategoryProductListPage";
-import ProductDisplayPage from "/src/pages/ProductDisplayPage";
-import CartPage from "/src/pages/CartPage";
-import CheckoutPage from "/src/pages/CheckoutPage";
-import Success from "/src/pages/Success";
-import Cancel from "/src/pages/Cancel";
-import OrderTrackingPage from "/src/pages/OrderTrackingPage";
+import Home from "/src/pages/Home.jsx";
+import AboutPage from "/src/pages/About.jsx";
+import SearchPage from "/src/pages/SearchPage.jsx";
+import CategoryProductListPage from "/src/pages/CategoryProductListPage.jsx";
+import ProductDisplayPage from "/src/pages/ProductDisplayPage.jsx";
+import CartPage from "/src/pages/CartPage.jsx";
+import CheckoutPage from "/src/pages/CheckoutPage.jsx";
+import Success from "/src/pages/Success.jsx";
+import Cancel from "/src/pages/Cancel.jsx";
+import OrderTrackingPage from "/src/pages/OrderTrackingPage.jsx";
 
 // --- AUTH PAGES ---
-import Login from "/src/pages/Login";
-import Register from "/src/pages/Register";
-import ForgotPassword from "/src/pages/ForgotPassword";
-import OtpVerification from "/src/pages/OtpVerification";
-import ResetPassword from "/src/pages/ResetPassword";
+import Login from "/src/pages/Login.jsx";
+import Register from "/src/pages/Register.jsx";
+import ForgotPassword from "/src/pages/ForgotPassword.jsx";
+import OtpVerification from "/src/pages/OtpVerification.jsx";
+import ResetPassword from "/src/pages/ResetPassword.jsx";
 
 // --- USER DASHBOARD PAGES ---
-import UserMenuMobile from "/src/pages/UserMenuMobile";
-import Profile from "/src/pages/Profile";
-import MyOrders from "/src/pages/MyOrders";
-import Address from "/src/pages/Address";
-import CategoryPage from "/src/pages/CategoryPage";
-import SubCategoryPage from "/src/pages/SubCategoryPage";
-import UploadProduct from "/src/pages/UploadProduct";
-import ProductAdmin from "/src/pages/ProductAdmin";
+import UserMenuMobile from "/src/pages/UserMenuMobile.jsx";
+import Profile from "/src/pages/Profile.jsx";
+import MyOrders from "/src/pages/MyOrders.jsx";
+import Address from "/src/pages/Address.jsx";
+import CategoryPage from "/src/pages/CategoryPage.jsx";
+import SubCategoryPage from "/src/pages/SubCategoryPage.jsx";
+import UploadProduct from "/src/pages/UploadProduct.jsx";
+import ProductAdmin from "/src/pages/ProductAdmin.jsx";
 
 // --- AGENT PAGES ---
-import AgentRegister from "/src/pages/agent/AgentRegister";
-import AgentDashboard from "/src/pages/agent/AgentDashboard";
+import AgentRegister from "/src/pages/agent/AgentRegister.jsx";
+import AgentDashboard from "/src/pages/agent/AgentDashboard.jsx";
 
 // --- EATERY PAGES ---
-import EateryLogin from "/src/pages/eatery/EateryLogin";
-import EateryRegister from "/src/pages/eatery/EateryRegister";
-import EateryDashboard from "/src/pages/eatery/EateryDashboard";
-import EateryMenuPage from "/src/pages/EateryMenuPage";
-import ManageProducts from "/src/pages/eatery/ManageProducts";
-import ViewOrders from "/src/pages/eatery/ViewOrders";
+import EateryLogin from "/src/pages/eatery/EateryLogin.jsx";
+import EateryRegister from "/src/pages/eatery/EateryRegister.jsx";
+import EateryDashboard from "/src/pages/eatery/EateryDashboard.jsx";
+import EateryMenuPage from "/src/pages/EateryMenuPage.jsx";
+import ManageProducts from "/src/pages/eatery/ManageProducts.jsx";
+import ViewOrders from "/src/pages/eatery/ViewOrders.jsx";
 import EateryProfile from "/src/pages/eatery/EateryProfile.jsx";
 
 
