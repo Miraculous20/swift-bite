6e36debf7fa334d44359f24cf6d838a4c6cd0d14
Miraculous20 <miraculous15052003@gmail.com>
2025-09-16 10:46:23 +0100

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
           </Route>
         </Routes>
       </BrowserRouter>
